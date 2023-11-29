0006c7ae3d0ec026edf3267c4fb34d27cbe705cd
elmobbB <108806956+elmobbB@users.noreply.github.com>
2023-11-29 09:53:58 +0800

    test enemy obstacle

---
+++ ShooterGame/src/test/Test_boss.java
@@ -0,0 +1,211 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.datatype.DatatypeConstants.Field;
+
+import org.junit.Test;
+
+import core.Enemy_Boss;
+import main.GameUI;
+
+public class Test_boss {
+	@Test
+	public void testEnemyBossConstruction() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Create an Enemy_Boss object
+		int x = 400;
+		int y = 200;
+		String type = "enemy02";
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Verify that the type was set correctly
+		assertEquals("enemy", enemyBoss.getID());
+
+	}
+
+	@Test
+	public void testEnemyIfDie() {
+
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Create an Enemy_Boss object
+		int x = 400;
+		int y = 200;
+		String type = "enemy02";
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		enemyBoss.reduceHP(30);
+
+		// Call the checkIfDie method
+		int result = enemyBoss.checkIfDie();
+
+		// Verify that the method returns the expected value
+		assertEquals(3343, result);
+	}
+
+	@Test
+	public void testEnemyIfNotDie() {
+
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Create an Enemy_Boss object
+		int x = 400;
+		int y = 200;
+		String type = "enemy02";
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		enemyBoss.reduceHP(1);
+
+		// Call the checkIfDie method
+		int result = enemyBoss.checkIfDie();
+
+		// Verify that the method returns the expected value
+		assertEquals(-1, result);
+	}
+
+	@Test
+	public void testUpdate_BossSpeedX_LargerThan0()
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "enemy02";
+
+		// Create an Enemy_Boss object
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Use reflection to set the value of speed_x to a positive value
+
+		java.lang.reflect.Field speedXField = Enemy_Boss.class.getDeclaredField("speed_x");
+		speedXField.setAccessible(true);
+		speedXField.setDouble(enemyBoss, 1);
+
+		// Call the update method
+		enemyBoss.update();
+
+		// Verify that the x-coordinate has increased
+		assertEquals(x + 1, enemyBoss.getX(), 2);
+	}
+
+	@Test
+	public void testUpdate_BossSpeedX_SmallerThan0()
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "enemy02";
+
+		// Create an Enemy_Boss object
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Use reflection to set the value of speed_x to a positive value
+
+		java.lang.reflect.Field speedXField = Enemy_Boss.class.getDeclaredField("speed_x");
+		speedXField.setAccessible(true);
+		speedXField.setDouble(enemyBoss, -1);
+
+		// Call the update method
+		enemyBoss.update();
+
+		// Verify that the x-coordinate has increased
+		assertEquals(x - 1, enemyBoss.getX(), -1);
+	}
+
+	@Test
+	public void testUpdate_BossSpeedX_ReverseWhenXLessThanOrEqualToRandomLeftOr0()
+			throws NoSuchFieldException, IllegalAccessException {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "enemy02";
+
+		// Create an Enemy_Boss object
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Use reflection to set the value of speed_x to a negative value
+		java.lang.reflect.Field speedXField = Enemy_Boss.class.getDeclaredField("speed_x");
+		speedXField.setAccessible(true);
+		speedXField.setDouble(enemyBoss, -1);
+
+		// Set randomLeft to a value greater than x to trigger the condition
+		java.lang.reflect.Field randomLeftField = Enemy_Boss.class.getDeclaredField("randomLeft");
+		randomLeftField.setAccessible(true);
+		randomLeftField.setInt(enemyBoss, x + 1);
+
+		// Call the update method
+		enemyBoss.update();
+
+		// Verify that the speed_x has reversed
+		double updatedSpeedX = (double) speedXField.getDouble(enemyBoss);
+		assertEquals(1, updatedSpeedX, 0.001);//value of 0.001 is used to provide a tolerance for comparing floating-point values
+	}
+
+	@Test
+	public void testUpdate_BossSpeedY_ReverseWhenYLessThanOrEqualTo10()
+			throws NoSuchFieldException, IllegalAccessException {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 9; // Set y to 10 to trigger the condition
+		String type = "enemy02";
+
+		// Create an Enemy_Boss object
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Use reflection to set the value of speed_y to a negative value
+		java.lang.reflect.Field speedYField = Enemy_Boss.class.getDeclaredField("speed_y");
+		speedYField.setAccessible(true);
+		speedYField.setDouble(enemyBoss, -1);
+
+		// Call the update method
+		enemyBoss.update();
+
+		// Verify that the speed_y has reversed
+		double updatedSpeedY = (double) speedYField.getDouble(enemyBoss);
+		assertEquals(1, updatedSpeedY, 0.001); //value of 0.001 is used to provide a tolerance for comparing floating-point values
+	}
+
+	@Test
+	public void testUpdate_SpeedXReversedWhenXPlusWidthReachedRandomRight()
+			throws NoSuchFieldException, IllegalAccessException {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 1000;
+		int y = 9; // Set y to 10 to trigger the condition
+		String type = "enemy02";
+
+		// Create an Enemy_Boss object
+		Enemy_Boss enemyBoss = new Enemy_Boss(gameUI, x, y, type);
+
+		// Use reflection to set the value of speed_y to a negative value
+		java.lang.reflect.Field speedYField = Enemy_Boss.class.getDeclaredField("speed_y");
+		speedYField.setAccessible(true);
+		speedYField.setDouble(enemyBoss, -1);
+
+		// Call the update method
+		enemyBoss.update();
+
+		// Verify that the speed_y has reversed
+		double updatedSpeedY = (double) speedYField.getDouble(enemyBoss);
+		assertEquals(1, updatedSpeedY, 0.001);//value of 0.001 is used to provide a tolerance for comparing floating-point values
+	}
+}
---
+++ ShooterGame/src/test/Test_obstacle.java
@@ -0,0 +1,180 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Method;
+
+import javax.xml.datatype.DatatypeConstants.Field;
+
+import org.junit.Test;
+
+import core.Enemy;
+import core.JetFighter;
+import core.Obstacle;
+import globalData.Constant;
+import globalData.Renderable;
+import globalData.Updateable;
+import main.GameUI;
+
+public class Test_obstacle {
+	@Test
+	public void testObstacleGetImage1() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "obstacle01";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Verify that the x and y coordinates were set correctly
+		assertEquals(x, obstacle.getX(), 0.01);
+		assertEquals(y, obstacle.getY(), 0.01);
+
+		// Verify that the type was set correctly
+		assertEquals("obstacle", obstacle.getID());
+
+	}
+
+	@Test
+	public void testObstacleGetImage2() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "obstacle02";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Verify that the x and y coordinates were set correctly
+		assertEquals(x, obstacle.getX(), 0.01);
+		assertEquals(y, obstacle.getY(), 0.01);
+
+		// Verify that the type was set correctly
+		assertEquals("obstacle", obstacle.getID());
+
+	}
+
+	@Test
+	public void testObstacleGetImage3() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 200;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Verify that the x and y coordinates were set correctly
+		assertEquals(x, obstacle.getX(), 0.01);
+		assertEquals(y, obstacle.getY(), 0.01);
+
+		// Verify that the type was set correctly
+		assertEquals("obstacle", obstacle.getID());
+
+	}
+
+	@Test
+	public void testObstacleUpdate() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 1000;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Call the update method
+		obstacle.update();
+
+		// Verify that the enemy has moved out of the screen
+		assertTrue(obstacle.getY() >= Constant.screenHeight);
+	}
+
+	@Test
+	public void TestObstacleRenderable() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 1000;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Call the update method
+		assertEquals(obstacle, obstacle.getRenderable());
+
+	}
+
+	@Test
+	public void TestObstacleGetWidth() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 1000;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+		assertEquals(Constant.tileSize, obstacle.getWidth(), 0);
+	}
+
+	@Test
+	public void TestObstacleGetHeight() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 1000;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+		assertEquals(Constant.tileSize, obstacle.getHeight(), 0);
+	}
+
+	@Test
+	public void TestObstacle_Draw() {
+		// Create a GameUI object
+		GameUI gameUI = new GameUI();
+
+		// Define test values
+		int x = 100;
+		int y = 1000;
+		String type = "obstacle03";
+
+		// Create an Obstacle object
+		Obstacle obstacle = new Obstacle(gameUI, x, y, type);
+
+		// Set up a BufferedImage
+		BufferedImage image = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+
+		// Create a Graphics2D object
+		Graphics2D g2 = image.createGraphics();
+
+		// Call the draw method
+		obstacle.draw(g2);
+	}
+
+}
